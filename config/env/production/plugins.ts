28376c92084edf41273e5bc4b288dd0084f14c48
BaptGuilhas <baptiste216@msn.com>
2025-03-26 16:37:18 +0100

    remove content in /env/prodution/plugins

--- config/env/production/plugins.ts
+++ config/env/production/plugins.ts
@@ -1,30 +1,30 @@
-    // ~/strapi-aws-s3/backend/config/plugins.js
+//     // ~/strapi-aws-s3/backend/config/plugins.js
     
-    module.exports = ({ env }) => ({
-      'users-permissions': {
-        config: {
-          jwtSecret: env('JWT_SECRET'), // Utilisation de la variable d'environnement
-        },
-      },
-      upload: {
-        config: {
-          provider: 'aws-s3',
-          providerOptions: {
-            credentials: {
-              accessKeyId: env('AWS_ACCESS_KEY_ID'),
-              secretAccessKey: env('AWS_ACCESS_SECRET'),
-            },
-            region: env('AWS_REGION'),
-            params: {
-              ACL: null,
-              Bucket: env('AWS_BUCKET'),
-            },
-          },
-          actionOptions: {
-            upload: {},
-            uploadStream: {},
-            delete: {},
-          },
-        },
-      },
-});
+//     module.exports = ({ env }) => ({
+//       'users-permissions': {
+//         config: {
+//           jwtSecret: env('JWT_SECRET'), // Utilisation de la variable d'environnement
+//         },
+//       },
+//       upload: {
+//         config: {
+//           provider: 'aws-s3',
+//           providerOptions: {
+//             credentials: {
+//               accessKeyId: env('AWS_ACCESS_KEY_ID'),
+//               secretAccessKey: env('AWS_ACCESS_SECRET'),
+//             },
+//             region: env('AWS_REGION'),
+//             params: {
+//               ACL: null,
+//               Bucket: env('AWS_BUCKET'),
+//             },
+//           },
+//           actionOptions: {
+//             upload: {},
+//             uploadStream: {},
+//             delete: {},
+//           },
+//         },
+//       },
+// });
