10ed00e6a7dd245544e6f9429da67b3e1dee9e65
BaptGuilhas <baptiste216@msn.com>
2025-03-30 13:04:28 +0200

    add content in /config/env/prodution/plugins

--- config/env/production/plugins.ts
+++ config/env/production/plugins.ts
@@ -1,32 +1,34 @@
-export default () => ({});
-
-//     // ~/strapi-aws-s3/backend/config/plugins.js
-    
-//     module.exports = ({ env }) => ({
-//       'users-permissions': {
-//         config: {
-//           jwtSecret: env('JWT_SECRET'), // Utilisation de la variable d'environnement
-//         },
-//       },
-//       upload: {
-//         config: {
-//           provider: 'aws-s3',
-//           providerOptions: {
-//             credentials: {
-//               accessKeyId: env('AWS_ACCESS_KEY_ID'),
-//               secretAccessKey: env('AWS_ACCESS_SECRET'),
-//             },
-//             region: env('AWS_REGION'),
-//             params: {
-//               ACL: null,
-//               Bucket: env('AWS_BUCKET'),
-//             },
-//           },
-//           actionOptions: {
-//             upload: {},
-//             uploadStream: {},
-//             delete: {},
-//           },
-//         },
-//       },
+// export default () => ({
+     
+    module.exports = ({ env }) => ({
+        'users-permissions': {
+          config: {
+            jwtSecret: env('JWT_SECRET'), // Utilisation de la variable d'environnement
+          },
+        },
+        upload: {
+          config: {
+            provider: 'aws-s3',
+            providerOptions: {
+              credentials: {
+                accessKeyId: env('AWS_ACCESS_KEY_ID'),
+                secretAccessKey: env('AWS_ACCESS_SECRET'),
+              },
+              region: env('AWS_REGION'),
+              params: {
+                ACL: null,
+                Bucket: env('AWS_BUCKET'),
+              },
+            },
+            actionOptions: {
+              upload: {},
+              uploadStream: {},
+              delete: {},
+            },
+          },
+        },
+  });
 // });
+
+    // ~/strapi-aws-s3/backend/config/plugins.js
+   
